6e329902b953153b33c8d66ee3a2e8fce12d0449
rohith1005H <rohithk1104@gmail.com>
2024-06-26 11:17:16 +0530

    just a little spice

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const btn = document.querySelector('#btn');
     const size = document.querySelector('#size');
 
-    
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 
     function createGrid(num) {
         container.innerHTML = '';
@@ -11,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const cell = document.createElement('div');
             cell.innerText = (i+1);
             cell.addEventListener('mouseenter', () => {
-                cell.style.backgroundColor = 'black';
+                cell.style.backgroundColor = getRandomColor();
             });
             cell.addEventListener('mouseleave', () => {
                 cell.style.backgroundColor = '#ffffff';
@@ -19,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
             cell.classList.add('cell');
             container.appendChild(cell);
         }
-        
+
         container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     }
